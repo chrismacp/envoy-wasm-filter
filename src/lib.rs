9cbdd3cb1c7ae578bf735d5b9aa435edac33f7f9
Chris MacPherson <chris@macpherson.dev>
2021-08-26 15:33:51 +0200

    Working version with envoy 1.17 and CLI flag to enable v2
    Need to do following to build the release - using wasme for this at the moment
    
    ```
    $ cargo build --target wasm32-unknown-unknown --release
    
    $ wasme build precompiled target/wasm32-unknown-unknown/release/authfilter.wasm --tag authfilter:v0.9
    
    $ wasme deploy envoy authfilter:v0.9 --envoy-image=envoyproxy/envoy-alpine:v1.17-latest --envoy-run-args "--bootstrap-version 2"
    ```

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+use log::info;
+use proxy_wasm as wasm;
+use proxy_wasm::types::Action;
+use wasm::types::LogLevel;
+
+#[no_mangle]
+pub fn _start() {
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_http_context(
+        |context_id, _root_context_id| -> Box<dyn wasm::traits::HttpContext> {
+            Box::new(HelloWorld { context_id })
+        },
+    )
+}
+
+struct HelloWorld {
+    context_id: u32,
+}
+
+impl wasm::traits::Context for HelloWorld {}
+
+impl wasm::traits::HttpContext for HelloWorld {
+    fn on_http_request_headers(&mut self, num_headers: usize) -> Action {
+        info!("Got {} HTTP headers in #{}.", num_headers, self.context_id);
+        
+        let headers = self.get_http_request_headers();
+        
+        let mut authority = "";
+        
+        for (name, value) in &headers {
+            info!("Header {} = {}", name, value);
+            if name == ":authority" {
+                authority = value;
+            }
+        }
+
+        self.set_http_request_header("x-hello", Some(&format!("Hello world from {}", authority)));
+
+        Action::Continue
+    }
+
+    fn on_http_response_headers(&mut self, _: usize) -> Action {
+        self.set_http_response_header("macp-goodbye", Some("to you sir"));
+        Action::Continue
+    }
+}
